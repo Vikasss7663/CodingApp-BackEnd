42bc5a3594207a44c9814ded6ac152cefb858761
Vishal Singh <vishalsinghgk2018@gmail.com>
2021-12-07 21:20:02 +0530

    updating folder

--- public/code/python/5.py
+++
@@ -1,7 +0,0 @@
-# print natural number upto n
-n = int(input("Enter n :- "))
-for i in range(1,n+1):
-    # seperated by space
-    # by default print seperate by new line
-    print(i, end=' ') 
-print("End")
