1b79568c3607809384af4bbe71c04e9b90dd80ab
Vishal Singh <vishalsinghgk2018@gmail.com>
2021-12-07 19:00:45 +0530

    test file added

---
+++ test_file/1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    printf("Hello World!!!.\n");
+    printf("This is a c programming test file.\n");
+    printf("Welcome to C Programming.\n");
+    return 0;
+}
---
+++ test_file/1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    cout << "Hello World!!!" << endl;
+    string name[] = {"Welcome","C++","Programming."};
+    for(int i=0;i<name.length;i++) {
+        cout << name[i] << " ";
+    } 
+    cout << endl;
+    cout << "Hope you like C++ Programming." << endl;
+    return 0;
+}
