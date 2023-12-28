9fd9211f87d12e7186ddf7c04c47c9e7239e756f
Vikasss7663 <vishalsinghgk2018@gmail.com>
2023-12-28 22:36:27 +0530

    online compiler fix

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ import ProblemRouter from './src/routes/ProblemRouter.js';
 import SubQuestionRouter from './src/routes/SubQuestionRouter.js';
 import ObjQuestionRouter from './src/routes/ObjQuestionRouter.js';
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 const app = express();
 const currPath = dirname(new URL(import.meta.url).pathname);
-const basePath = "public/code";
+const basePath = join(currPath, "/temp");
 
 app.engine('html', renderFile);
 app.set('view engine', 'ejs');
@@ -37,7 +37,7 @@ app.listen(port, () => {
     console.log("Server is listening at port ", port);
 });
 
-app.post('/run', (req, res) => {
+app.post('/run', async (req, res) => {
     const lang = req.body.lang;
     const userId = req.body.userId;
     const code = req.body.code;
@@ -58,6 +58,13 @@ app.post('/run', (req, res) => {
 });
 
 function getFileName(lang, basePath, userId) {
+
+    const userDirectory = join(basePath, userId);
+    
+    if (!fs.existsSync(userDirectory)) {
+        fs.mkdirSync(userDirectory, { recursive: true });
+    }
+
     switch(lang) {
         case "C":
             return basePath + "/" + userId + ".c";
@@ -75,69 +82,85 @@ function getFileName(lang, basePath, userId) {
 function compileCode(lang,userId,s,res) {
     if(lang == "C") {
         const fileName = join(basePath, "/"+userId+".c");
-        const outputFileName = join(basePath, "/"+userId);
-        console.log(fileName);
-        console.log(outputFileName);
+        const outputFileName = join(basePath, "/"+userId+".out");
         const compile = spawn(`gcc ${fileName} -o ${outputFileName} && ${outputFileName}`,{shell: true});
         s.pipe(compile.stdin);
         compile.stdout.on('data', (data) => {
-            console.log(data.toString());
-            res.send(data.toString());
+            fs.unlinkSync(fileName);
+            res.write(data.toString());
             res.end();
-            return;
         });
         compile.stderr.on('data', (data) => {
-            console.log(data.toString());
-            res.send(data.toString());
+            fs.unlinkSync(fileName);
+            res.write(data.toString());
             res.end();
-            return;
         });
     } else if(lang == "C++") {
         const fileName = join(basePath,"/"+userId+".cpp");
-        const outputFileName = join(basePath, "/"+userId);
+        const outputFileName = join(basePath, "/"+userId+".out");
         const compile = spawn(`g++ ${fileName} -o ${outputFileName} && ${outputFileName}`,{shell: true});
         s.pipe(compile.stdin);
         compile.stdout.on('data', (data) => {
-            console.log(data.toString());
-            res.send(data.toString());
+            fs.unlinkSync(fileName);
+            res.write(data.toString());
             res.end();
         });
         compile.stderr.on('data', (data) => {
-            console.log(data.toString());
-            res.send(data.toString());
+            fs.unlinkSync(fileName);
+            res.write(data.toString());
             res.end();
         });
     } else if(lang == "Java") {
-        const fileName = basePath+"/"+userId+"/Solution.java";
-        const outputFileName = join(basePath, "/" + userId + "/Solution");
-        const compile = spawn(`javac ${fileName} && java ${outputFileName}`,{shell: true});
+        const fileName = join(basePath, "/" + userId + "/Solution.java");
+        const outputDirName = join(basePath, "/" + userId);
+        const compile = spawn(`javac ${fileName} && java -cp ${outputDirName} Solution`,{shell: true});
         s.pipe(compile.stdin);
         compile.stdout.on('data', (data) => {
-            console.log(data.toString());
-            res.send(data.toString());
+            fs.unlinkSync(fileName);
+            res.write(data.toString());
             res.end();
         });
         compile.stderr.on('data', (data) => {
-            console.log(data.toString());
-            res.send(data.toString());
+            fs.unlinkSync(fileName);
+            res.write(data.toString());
             res.end();
-            // process.exit();
         });
     } else if(lang == "Python") {
         const fileName = join(basePath, "/"+userId+".py");
         const compile = spawn(`python3 ${fileName}`,{shell: true});
+        console.log("compiling python code")
         s.pipe(compile.stdin);
         compile.stdout.on('data', (data) => {
+            fs.unlinkSync(fileName);
             res.write(data.toString());
-            // res.end();
+            res.end();
         });
         compile.stderr.on('data', (data) => {
+            fs.unlinkSync(fileName);
             res.write(data.toString());
             res.end();
-            // process.exit();
         });
-        compile.on('exit', function() {
-            res.end();
-        });
+    } else {
+        res.write("Language Not Supported");
+        res.end();
     }
 }
+
+/*
+
+curl -X POST \
+    http://localhost:3000/run \
+    -H 'Content-Type: application/json' \
+    -d '{
+        "lang": "Java",
+        "userId": "2",
+        "code": "public class Solution { public static void main(String[] args) { System.out.println(\"Hello, World! How are you !\"); } }",
+        "input": ""
+    }'
+
+
+curl -X POST -H "Content-Type: application/json" -d '{"lang": "Python", "code": "print(\"Hello from Python!\")", "userId": "1" }' http://localhost:3000/run
+
+curl -X POST -H "Content-Type: application/json" -d '{"lang": "C++", "code": "#include <iostream>\nint main() { std::cout << \"Hello, World!\" << std::endl; return 0; }", "userId": "2"}' http://localhost:3000/run
+
+*/
